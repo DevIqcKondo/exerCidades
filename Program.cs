9b093a42c3fa1420576d6863669152380443683b
Isadora Queiroz <KondoIqcDev@gmail.com>
2021-03-12 13:20:55 -0300

    Execusão do exercício

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace exercidades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("------------------------");
+            Console.WriteLine("Cidades do Litoral Sul:");
+            Console.WriteLine("------------------------");
+            Console.ForegroundColor=ConsoleColor.DarkCyan;
+            Console.WriteLine("Peruíbe");
+            Console.ForegroundColor=ConsoleColor.DarkGreen;
+            Console.WriteLine("Itanhaém");
+            Console.ForegroundColor=ConsoleColor.DarkRed;
+            Console.WriteLine("Mongagua");
+            Console.ForegroundColor=ConsoleColor.DarkYellow;
+            Console.WriteLine("Praia Grande");
+            Console.ForegroundColor=ConsoleColor.Gray;
+            Console.WriteLine("São Vicente");
+            Console.ForegroundColor=ConsoleColor.Magenta;
+            Console.WriteLine("Santos");
+            Console.ResetColor();
+        
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# exerCidades
+# Exercícios Cidades
 Faça um programa que exiba os nomes das cidades do litoral sul de São Paulo, um em cada linha.
